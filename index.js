fce5f654c0b0ffd200cc00be7a071dc2782724d4
Shiratie Prodhan <shiratiee@gmail.com>
2018-05-31 18:20:22 -0400

    curious to see the changes

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const router = require('./public/index.js')
+const path = require('path');
+const app = express()
 
-
-
-let app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-app.use('/', router);
 
 app.listen(process.env.PORT || 8080,function() {
     console.log('Keeping it 8080!')
 })
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
+
+  // sends game.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/Main/game.html'))
+  })
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+
