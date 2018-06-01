60ba6cc7f9053bc33b91e2d85c769da8bfda8e6b
Shiratie Prodhan <shiratiee@gmail.com>
2018-06-01 03:21:44 -0400

    cleaning up

--- index.js
+++ index.js
@@ -15,34 +15,29 @@ app.listen(process.env.PORT || 8080,function() {
     console.log('Keeping it 8080!')
 })
 
-  // static file-serving middleware
-  app.use(express.static(path.join(__dirname, 'public')))
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, 'public')))
 
-   // sends game.html
-   router.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/Main/game.html'))
-  })
+// sends game.html
+router.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/Main/game.html'))
+})
 
-  // any remaining requests with an extension (.js, .css, etc.) send 404
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+  } else {
+    next()
+  }
+})
 
-  // sends game.html
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/Main/game.html'))
-  })
-
-  // error handling endware
-  app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-  })
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
 
